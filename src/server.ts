7f4875aef2bd139aa3dc5ff93f981f78d30543d9
EduSantosBrito <edu.santos.brito@gmail.com>
2020-03-14 16:21:57 -0300

    Setting up graphql

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response } from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import depthLimit from 'graphql-depth-limit';
+import compression from 'compression';
+import cors from 'cors';
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = { Query: { hello: () => 'Hello world!' } };
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hey');
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    validationRules: [depthLimit(7)],
+});
+
+app.use(cors());
+app.use(compression());
+apolloServer.applyMiddleware({ app });
+
+app.use('*', (req: Request, res: Response) => {
+    res.status(404).send('404');
 });
 
 app.listen(app.get('port'), () => {
